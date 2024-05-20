b4d014753f379238ee8c613658d4be7e57f9b4e1
Elton133 <134627073+Elton133@users.noreply.github.com>
2024-05-20 17:36:03 +0000

    Tour Ghana Web App

--- travelblog.js
+++ travelblog.js
@@ -1,45 +1,12 @@
-/*document.addEventListener("DOMContentLoaded", function () {
-  const fadeElements = document.querySelectorAll(".fade-in, .fade-out");
-
-  function checkFade() {
-    fadeElements.forEach((element) => {
-      if (isElementInViewport(element)) {
-        element.classList.add("show");
-        element.classList.remove("hide");
-      } else {
-        element.classList.add("hide");
-        element.classList.remove("show");
-      }
-    });
-  }
-
-  function isElementInViewport(el) {
-    const rect = el.getBoundingClientRect();
-    return (
-      rect.top >= 0 &&
-      rect.left >= 0 &&
-      rect.bottom <=
-        (window.innerHeight || document.documentElement.clientHeight) &&
-      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-    );
-  }
-
-  // Listen for scroll events and check if elements are in view
-  window.addEventListener("scroll", checkFade);
-  // Check for fade elements initially
-  checkFade();
-});
-*/
-
 window.addEventListener("scroll", reveal);
 
 function reveal() {
-  var reveals = document.querySelectorAll(".reveal");
+  let reveals = document.querySelectorAll(".reveal");
 
-  for (var i = 0; i < reveals.length; i++) {
-    var windowheight = window.innerHeight;
-    var revealtop = reveals[i].getBoundingClientRect().top;
-    var revealpoint = 150;
+  for (let i = 0; i < reveals.length; i++) {
+    let windowheight = window.innerHeight;
+    let revealtop = reveals[i].getBoundingClientRect().top;
+    let revealpoint = 150;
 
     if (revealtop < windowheight - revealpoint) {
       reveals[i].classList.add("active");
